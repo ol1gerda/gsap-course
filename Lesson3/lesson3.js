0be095b6003335793d7a875c823fd1100c6afe7e
ol1gerda <olgerda@gmail.com>
2024-03-13 14:35:40 +0700

    Lesson3

--- Lesson3/lesson3.js
+++ Lesson3/lesson3.js
@@ -1,8 +1,8 @@
 let titleProject = 'Оценка стоимости';
 let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
-let screenPrice = '10000';
-let percentage = '10';
-let fullPrice = '100000';
-let responsive = 'true';
+let screenPrice = 10000;
+let percentage = 10;
+let fullPrice = 100000;
+let responsive = true;
 
 console.log('Название проекта', titleProject, 'по цене', screenPrice, 'за 1 экран')
