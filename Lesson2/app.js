07ddf54f0a5763774120a1aa7b585e295d97cb24
ol1gerda <olgerda@gmail.com>
2024-03-13 17:39:21 +0700

    Lesson4

--- Lesson2/app.js
+++ Lesson2/app.js
@@ -4,3 +4,9 @@ let city = 'Tomsk';
 let hobbies = 'Singing and taking photos'
 
 console.log('Hello,', myname, 'from', city)
+
+let titleProject = prompt('Название проекта?')
+let b = 
+let res = a + b
+
+console.log(res)
