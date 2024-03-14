b476786aa5a6790841e31e2189106d7cfe6a85c6
ol1gerda <olgerda@gmail.com>
2024-03-14 08:37:15 +0700

    Lesson4

--- Lesson4/lesson4.js
+++ Lesson4/lesson4.js
@@ -1,40 +1,33 @@
-let titleProject = 'Оценка стоимости проекта: ';
-let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+// let titleProject = 'Оценка стоимости проекта: ';
+// let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
 let screenPrice = 10000;
 let percentage = 10;
-let fullPrice = 100000;
-let responsive = true;
+// let fullPrice = 100000;
+// let responsive = true;
 
 // Урок 4. Задание 1-3.
 
-let first = prompt('Название проекта?');
-let res = titleProject + first;
-let second = prompt('Уровень сложности?');
-screensValue = second;
-let third = prompt ('Нужна ли адаптивность?');
-responsive = third;
+let titleProject = prompt('Название проекта?');
+let screensValue = prompt('Шаблонные, с уникальным дизайном, с анимациями?');
+let responsive = confirm('Нужна ли адаптивность?');
 
 
 // Урок 4. Задание 4.
 
-let firstq = prompt('Какой сервис нужен?');
-let service1 = firstq;
-let secondq = prompt('Сколько это будет стоить?');
-let servicePrice1 = +secondq;
-let thirdq = prompt ('Какой еще сервис тебе нужен?');
-let service2 = thirdq;
-let fourthq = prompt ('Сколько будет стоить этот второй сервис?');
-let servicePrice2 = +fourthq;
+let service1 = prompt('Какой сервис нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой еще сервис тебе нужен?');
+let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?');
 
 
 // Урок 4. Задание 5.
 
-let fullPrice1 = screenPrice + servicePrice1 + servicePrice2;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 
 // Урок 4. Задание 6.
 
-let servicePercentPrice = Math.round( fullPrice1 - (fullPrice1*0.15) );
+let servicePercentPrice = Math.round( fullPrice - (fullPrice * percentage / 100) );
 
 
 // Урок 4. Задание 7.
@@ -45,17 +38,19 @@ let servicePercentPrice = Math.round( fullPrice1 - (fullPrice1*0.15) );
 // Если отрицательное значение то вывести “Что то пошло не так”
 // Не забудьте учитывать варианты, если цена равна 0, 20000, 50000 (в консоль вывести любой текст по желанию)
 
-if (fullPrice1 >= 50000) {
+if (fullPrice >= 50000) {
     result = 'сделаем скидку в 10%';
-  } else if (fullPrice1 >= 20000 && fullPrice1 < 50000) {
+  } else if (fullPrice >= 20000 && fullPrice < 50000) {
     result = 'сделаем скидку в 5%';
-  } else if (fullPrice1 >= 0 && fullPrice1 < 20000) {
+  } else if (fullPrice >= 0 && fullPrice < 20000) {
     result = 'скидка не предусмотрена';
-  } else if (fullPrice1 < 0) {
+  } else if (fullPrice < 0) {
     result = 'что-то пошло не так';
   }
 
 
-console.log(fullPrice1, result);
+console.log(titleProject, screensValue, responsive, 
+    service1, servicePrice1, service2, servicePrice2, 
+    fullPrice, servicePercentPrice, result);
 
 
